fa8f9f8325407e77dafe510bc5c2c4b6317b3c15
odulex <odusolajeremiah71@yahoo.com>
2026-01-07 15:14:05 +0100

    Added Cta button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,16 @@
 import { Carousel, Button } from "react-bootstrap";
 import "./Hero.css";
+import { useNavigate } from "react-router-dom";
 
 const slides = [
-    { image: "/images/image1.jpg", title: "BEHIND EVERY SHOES IS A STORY", subtitle: "In stock, ready to ship", cta: "" },
-    { image: "/images/image2.jpg", title: "ENGINEERED FOR LIFE", subtitle: "Limited release", cta: "" },
-    { image: "/images/image3.jpg", title: "BUILT DIFFERENT", subtitle: "Crafted to age", cta: "" },
+    { image: "/images/image1.jpg", title: "BEHIND EVERY SHOES IS A STORY", subtitle: "In stock, ready to ship", cta: "EXPLORE" },
+    { image: "/images/image2.jpg", title: "ENGINEERED FOR LIFE", subtitle: "Limited release", cta: "EXPLORE" },
+    { image: "/images/image3.jpg", title: "BUILT DIFFERENT", subtitle: "Crafted to age", cta: "EXPLORE" },
     { image: "/images/image4.jpg", title: "TIMELESS DESIGNS", subtitle: "Worldwide delivery", cta: "EXPLORE" },
 ];
 
 const Hero = () => {
+    const navigate = useNavigate();
     return (
         <>
             {/* ================== CAROUSEL ================== */}
@@ -33,7 +35,9 @@ const Hero = () => {
                                     {slide.subtitle}
                                 </small>
                                 <h1 className="hero-title">{slide.title}</h1>
-                                <Button variant="outline-light" className="hero-btn">
+                                <Button 
+                                onClick={()=>navigate('/shop')}
+                                variant="outline-light" className="hero-btn">
                                     {slide.cta}
                                 </Button>
                             </div>
