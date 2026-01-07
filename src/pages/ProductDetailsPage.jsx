2b5268650ec1c8db119b93157e3a23f90c072a63
odulex <odusolajeremiah71@yahoo.com>
2026-01-07 11:23:13 +0100

    made changes

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,5 +1,6 @@
-import ShopNavBar from "../components/shop/ShopNavBar";
+
 import ProductDetails from "../components/ProductDetails/ProductDetails";
+import ShopNavBar from "../components/Shop/ShopNavBar";
 
 const ProductDetailsPage = () => {
     return (
