baa3c3c5cea5cc6a2781c4307b3668a4caebd20e
odulex <odusolajeremiah71@yahoo.com>
2026-01-12 10:56:00 -0300

    Made changes at the front end

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 // Dummy data for demo
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  resolve: {
-    dedupe: ["react", "react-dom"],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://shoe-be.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
-})
+    resolve: {
+      dedupe: ["react", "react-dom"],
+    },
+  })
 
 
